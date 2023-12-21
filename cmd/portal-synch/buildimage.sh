c90d445e9a4c74953caefd6b2cb614cb27a8c094
congpeiqing <peiqing@nascentcore.ai>
2023-12-21 16:29:52 +0800

    fix lint

--- cmd/portal-synch/buildimage.sh
+++ cmd/portal-synch/buildimage.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 CGO_ENABLED=0  GOOS=linux  GOARCH=amd64  go build .
-docker build -f ./Dockerfile . -t sxwl-registry.cn-beijing.cr.aliyuncs.com/sxwl-ai/portalsynch:$(git rev-parse --short HEAD)
-docker push sxwl-registry.cn-beijing.cr.aliyuncs.com/sxwl-ai/portalsynch:$(git rev-parse --short HEAD)
+docker build -f ./Dockerfile . -t sxwl-registry.cn-beijing.cr.aliyuncs.com/sxwl-ai/portalsynch:"$(git rev-parse --short HEAD)"
+docker push sxwl-registry.cn-beijing.cr.aliyuncs.com/sxwl-ai/portalsynch:"$(git rev-parse --short HEAD)"
